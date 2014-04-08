7c7417d8ce929931d24f4dad3e0db59e44f9a01f
Tony Lee <github@roganartu.com>
2014-04-09 04:08:13 +1000

    Added basic dismissable warning page

--- heartbleedchecker.js
+++ heartbleedchecker.js
@@ -18,7 +18,6 @@ var heartbleedChecker = {
    * @public
    */
   checkHost: function(host) {
-    console.log(host);
     var url = this.baseURL + encodeURIComponent(host);
     var xhr = new XMLHttpRequest();
     xhr.open("GET", url, true);
@@ -40,8 +39,61 @@ var heartbleedChecker = {
     var xhr = e.target;
     if (xhr.readyState == 4) {
       var resp = JSON.parse(xhr.responseText);
-      console.log(resp);
+      if (/^SECURE/.test(resp.data)) {
+        // Do nothing, site secure
+      } else if (/^INSECURE/.test(resp.data)) {
+        console.log("Current host is vulnerable to Heartbleed OpenSSL bug CVE-2014-0160");
+        this.displayWarning();
+      } else {
+        console.log("Error determining Heartbleed vulnerability: " + resp.data);
+      }
     }
+  },
+
+  displayWarning: function() {
+    var div = document.createElement('div');
+    div.style.backgroundImage = "url('" + chrome.extension.getURL("images/semi-transparent.png") + "')";
+    div.style.width = "100%";
+    div.style.height = "100%";
+    div.style.position = "absolute";
+    div.style.left = "0";
+    div.style.top = "0";
+    div.style.zIndex = "9999";
+    div.id = "heartbleed-blackout";
+
+    // Load page content in from local store
+    var url = chrome.extension.getURL("insecure.html");
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.setRequestHeader("Content-Type", "text/html");
+    xhr.onreadystatechange = function (e) {
+      var xhr = e.target;
+      if (xhr.readyState == 4) {
+        div.innerHTML = xhr.responseText;
+      }
+    };
+    xhr.send(null);
+
+    // Append to body
+    document.body.appendChild(div);
+
+    this.bindWarning();
+  },
+
+  /**
+   * Binds acceptClick to accept warning button. Ensures element exists first.
+   *
+   * @public
+   */
+  bindWarning: function(e) {
+    if (document.getElementById("heartbleed-accept") === null) {
+      setTimeout(this.bindWarning, 50);
+      return;
+    }
+    document.getElementById("heartbleed-accept").addEventListener("click", function (e) {
+      // Hide display and remember that we hid it for future checks
+      document.getElementById("heartbleed-blackout").style.display = "none";
+    });
   },
 };
 
